7d027866a971814860577e25e2f996ad15ecf88c
a3811 <a3811@DESKTOP-CE1H9HQ>
2016-04-11 16:34:47 +0900

    add 뺄셈기능과 테스트 코드 추가
    기본적 기능

--- src/test/java/simcalc/SimpleCalculatorTest.java
+++ src/test/java/simcalc/SimpleCalculatorTest.java
@@ -14,5 +14,13 @@ public class SimpleCalculatorTest {
 		
 		assertEquals(30, calc.getResult());
 	}
+	
+	@Test
+	public void testSub(){
+		SimpleCalculator calc = new SimpleCalculator();
 
+		calc.sub(10,  20);
+		
+		assertEquals(-10, calc.getResult());
+	}
 }
